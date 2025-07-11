e7c3c1abbbe9234336eda04dd594ea1fcd623d9a
FilippoBau <filippo.baudanza@pagopa.it>
2025-07-11 11:18:56 +0200

    fix(scripts): add sleep after dangling case creation

--- scripts/azure/create_dangling_case.sh
+++ scripts/azure/create_dangling_case.sh
@@ -87,8 +87,8 @@ az network dns record-set cname show \
 
 
 
-echo "Waiting 30 seconds before deleting storage account..."
-sleep 30
+echo "Waiting 3 minutes before deleting storage account..."
+sleep 180
 
 
 echo "Deleting Storage Account to create dangling CNAME..."
