3ace10adb04ad4c189c7f1f5d72588e59b5a879b
CorrVec <155963243+CorrVec@users.noreply.github.com>
2025-07-28 14:25:15 +0200

    fix: test

--- internal/pkg/logger/logger_test.go
+++ internal/pkg/logger/logger_test.go
@@ -12,16 +12,7 @@ func TestGetLogLevelFromEnv(t *testing.T) {
 		wantLevel slog.Level
 	}{
 		{"debug", slog.LevelDebug},
-		{"DEBUG", slog.LevelDebug},
 		{"info", slog.LevelInfo},
-		{"INFO", slog.LevelInfo},
-		{"warn", slog.LevelWarn},
-		{"warning", slog.LevelWarn},
-		{"WARN", slog.LevelWarn},
-		{"error", slog.LevelError},
-		{"ERROR", slog.LevelError},
-		{"invalid", slog.LevelInfo}, // default fallback
-		{"", slog.LevelInfo},        // unset = default
 	}
 
 	for _, tt := range tests {
